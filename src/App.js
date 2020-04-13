ef6f0bdf6c641975781a890b354d38ee2c4b2fe5
tharindu4903 <tharindudhananjaya66@gmail.com>
2020-04-13 18:15:38 +0530

    12

--- src/App.js
+++ src/App.js
@@ -1,26 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Container from '@material-ui/core/Container';
+//import Grid from '@material-ui/core/Grid'
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Countdown from './component/countdown';
+import AppBar from '@material-ui/core/AppBar';
 
-function App() {
+
+
+class App extends Component { 
+
+  render() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     
+<body >
+  <AppBar position="sticky" className={'appbar'}>
+  <h2>අවුරුදු නැකත්</h2>
+  </AppBar>
+    <Container fixed maxWidth="sm" className={'abc'}>
+      <Typography component="div"  />
+      <div className={'card1'}>
+        <Card >
+          <CardContent>
+            <Typography class={'cardh'}>
+              පුණ්‍ය කාලය
+            </Typography>
+            <Typography variant="body2" component="p" class={'cbody'}>
+            අප්‍රේල් මස 13 වන සඳුදා දින අපරභාග 1 පසුවී විනාඩි 59 සිට එදින රාත්‍රී 2 පසුවී විනාඩි 47 දක්වා යෙදී ඇත.
+            </Typography>
+            <Typography variant="body2" component="p" className= {'count'}>
+            නැකතට තව,
+            </Typography>
+           <Countdown futureDate="2020-04-13 13:59:00"/>
+          </CardContent>
+        </Card>
+        
+      </div>
+      <div className={'card1'}>
+        <Card>
+          <CardContent>
+          <Typography class={'cardh'}>
+            අලුත් අවුරුදු උදාව
+            </Typography>
+            <Typography variant="body2" component="p" class={'cbody'}>
+            අප්‍රේල් මස 13 වන සඳුදා දින අපරභාග 08.23 ට සිංහල අලුත් අවුරුද්ද උදා වේ.
+            </Typography>
+            <Typography variant="body2" component="p" className= {'count'}>
+            නැකතට තව,
+            </Typography>
+           <Countdown futureDate="2020-04-13 20:23:00"/>
+          </CardContent>
+        </Card>
+        
+      </div>
+      <div className={'card1'}>
+        <Card>
+          <CardContent>
+          <Typography class={'cardh'}>
+          ආහාර පිසීම
+            </Typography>
+            <Typography variant="body2" component="p" class={'cbody'}>
+            අප්‍රේල් මස 13 වන සඳුදා දින අපරභාග 10.05 ට ශ්වේත වර්ණයෙන් සැරසී නැගෙනහිර දිශාව බලා ලිප් බැඳ ගිනි මොළවා කිරි මිශ්‍ර කිරිබතක් ද තල මිශ්‍ර කැවිලි වර්ගයක් සහ අග්ගලා පිළියෙල කරගැනීම මැනවි.
+            </Typography>
+            <Typography variant="body2" component="p" className= {'count'}>
+            නැකතට තව,
+            </Typography>
+           <Countdown futureDate="2020-04-13 22:05:00"/>
+          </CardContent>
+        </Card>
+        
+      </div>
+      <div className={'card1'}>
+        <Card>
+          <CardContent>
+          <Typography class={'cardh'}>
+          වැඩ ඇල්ලීම,ගනුදෙනු කිරීම හා ආහාර අනුභවය
+            </Typography>
+            <Typography variant="body2" component="p" class={'cbody'}>
+            අප්‍රේල් මස 13 වන සඳුදා දින අපරභාග 10.43 ට ශ්වේත වර්ණයෙන් සැරසී නැගෙනහිර දිශාව බලා සියලු වැඩ අල්ලා ගනුදෙනු කොට ආහාර අනුභව කිරීම මැනවි.
+            </Typography>
+            <Typography variant="body2" component="p" className= {'count'}>
+            නැකතට තව,
+            </Typography>
+           <Countdown futureDate="2020-04-13 22:43:00"/>
+          </CardContent>
+        </Card>
+        
+      </div>
+      <div>
+        <p>©2020 D'Front Creations. All rights reserved</p>
+      </div>
+     
+    </Container>
+</body>
+   
   );
 }
-
+}
 export default App;
---
+++ src/component/countdown.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+
+import moment, { duration } from "moment";
+
+import Typography from '@material-ui/core/Typography';
+import '../App.css';
+
+class countdown extends Component {
+
+
+  state = {
+    days: 0,
+    hours: 0,
+    mins: 0,
+    secs: 0
+
+  };
+
+  addZeros = value => {
+    value = String(value);
+    while (value.length < 2) {
+      value = `0${value}`;
+    }
+    return value;
+  };
+
+  setCountdown = () => {
+    const futureDate = moment(this.props.futureDate);
+
+    const today = moment();
+
+    const clockDuration = duration(futureDate.diff(today));
+
+    const days = Math.floor(clockDuration.asDays());
+    const hours = clockDuration.hours();
+    const mins = clockDuration.minutes();
+    const secs = clockDuration.seconds();
+
+    this.setState({
+      days,
+      hours,
+      mins,
+      secs
+    });
+  };
+  componentDidMount() {
+    this.setCountdown();
+    this.interval = setInterval(() => {
+      this.setCountdown();
+    }, 1000);
+
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+ 
+
+  render() {
+    if (this.state.secs<0) {
+      document.getElementById('my').style.display = "none"; 
+    }
+    return (
+      <div >
+          
+
+        <Typography class={'cardh'} id ="my" >
+          පැය {this.addZeros(this.state.hours)} විනාඩි {this.addZeros(this.state.mins)} තත්.{this.addZeros(this.state.secs)}
+
+        </Typography>
+
+      </div>
+
+    );
+
+  }
+}
+
+
+export default countdown;
